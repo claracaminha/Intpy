a95cffb9f2b4972260955554afc001b17564a18e
claracaminha <62616283+claracaminha@users.noreply.github.com>
2022-09-14 19:24:14 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -2,15 +2,16 @@ from intpy.intpy import initialize_intpy, deterministic
 import time
 import sys
 
+
 @deterministic
 def get_empirical_CVaR(rewards, alpha = 0.9):
     a = sorted((rewards).copy(), reverse= True)
     temp=[]
     for i in range(1,len(a)-10):
-        if float((len(a)) + 1) / len(a) >= 1-alpha:
+        if (i + 1 / len(a)) >= 1-alpha:
             temp.append((int(a[i])))
-     
-    return float(sum(temp) / len(a))     
+      
+    return float(sum(temp) / len(a))    
    
   
 
@@ -21,9 +22,11 @@ def main(rewards):
   
 
 if __name__ == "__main__":
-    n = (sys.argv[1:])
+    n = (sys.argv[2:])
     n.pop()
     n.pop()
+    for i in range(len(n)):
+      n[i]=n[i][:-1]
     print(n)
     start = time.perf_counter()
     main(n)
