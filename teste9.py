78624ae47838b9d2ead76b47f21f6d83f6dafe1d
claracaminha <62616283+claracaminha@users.noreply.github.com>
2022-09-23 21:27:30 -0300

    Update teste9.py

--- teste9.py
+++ teste9.py
@@ -23,11 +23,7 @@ def main(seq):
   
 
 if __name__ == "__main__":
-    seq = (sys.argv[2:])
-    seq.pop()
-    seq.pop()
-    for i in range(len(seq)):
-      seq[i]=seq[i][:-1]
+    seq = (sys.argv[1])
     start = time.perf_counter()
     main(seq)
     print(time.perf_counter()-start)
